c3449dae484081e6a4001c518352ffb52a90bdc8
prem-kumar-24 <150504284+prem-kumar-24@users.noreply.github.com>
2025-08-22 20:51:33 +0530

    Add files via upload
    Day 1

---
+++ Variable.js
@@ -0,0 +1,22 @@
+const Username="Prem"
+console.log(Username)
+console.log(typeof Username)
+
+var age=29
+console.log(age)
+console.log(typeof age)
+
+var Vote = true
+console.log(Vote)
+console.log(typeof Vote)
+
+if (Vote=true) {
+    let canVote=" is eligible to vote"
+    console.log(Username+ canVote)
+} else {
+    let canVote=" is not eligible to vote"
+    console.log(Username+ canVote)
+}
+let canVote
+console.log(canVote)
+console.log(typeof canVote)
